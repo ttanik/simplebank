ebf44c7f617e78b6148db92c74211d51a1dec881
Tulio Tanikawa <tulio_t@hotmail.com>
2023-05-04 08:09:08 -0300

    Fix add secrets manager pt 2

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 set -e
 
 echo "run db migration"
+echo $DB_SOURCE
+echo $DB_DRIVER
+echo $SERVER_ADDRESS
 source /app/app.yml
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 

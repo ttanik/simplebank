fc05e5198c5a25133ad54bb4d814443be00be213
Tulio Tanikawa <tulio_t@hotmail.com>
2023-05-07 07:55:43 -0300

    Feat changes from app.yml to app.env

--- util/config.go
+++ util/config.go
@@ -17,7 +17,7 @@ type Config struct {
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
-	viper.SetConfigType("yml")
+	viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
 	err = viper.ReadInConfig()
